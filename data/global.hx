73d4f2f075d1710805c4702c7e61e58759ac7636
ItsLJcool <54383469+ItsLJcool@users.noreply.github.com>
2025-01-03 17:11:58 -0700

    made it so you can add more keys lol

--- data/global.hx
+++ data/global.hx
@@ -1,10 +1,16 @@
 //a
 
 function new() {
+    FlxG.save.data.volumeUpKeys = null;
+    FlxG.save.data.volumeDownKeys = null;
+    FlxG.save.data.muteKeys = null;
+    
     FlxG.save.data.volumeUpKeys ??= FlxG.sound.volumeUpKeys;
     FlxG.save.data.volumeDownKeys ??= FlxG.sound.volumeDownKeys;
+    FlxG.save.data.muteKeys ??= FlxG.sound.muteKeys;
     FlxG.save.flush();
 
     FlxG.sound.volumeUpKeys = FlxG.save.data.volumeUpKeys;
     FlxG.sound.volumeDownKeys = FlxG.save.data.volumeDownKeys;
+    FlxG.sound.muteKeys = FlxG.sound.muteKeys;
 }
--- data/states/KeybindsOptions.hx
+++ data/states/KeybindsOptions.hx
@@ -4,53 +4,43 @@ import flixel.input.keyboard.FlxKey;
 import flixel.effects.FlxFlicker;
 import funkin.options.Options;
 
-var __volumeUp = "Vol Up";
-var __volumeDown = "Vol Down";
+import Reflect;
+
+var __data = [
+    {
+        name: "Vol Up",
+        save: "volumeUpKeys",
+    }, {
+        name: "Vol Down",
+        save: "volumeDownKeys",
+    }, {
+        name: "Mute",
+        save: "muteKeys",
+    }
+];
+
 function new() {
+    var settingsPEAK = [];
+    for (item in __data) settingsPEAK.push({ name: item.name, control: 'SWITCHMOD' });
     categories.push({
         name: 'Volume',
-        settings: [
-            {
-                name: __volumeUp,
-                control: 'SWITCHMOD'
-            }, {
-                name: __volumeDown,
-                control: 'SWITCHMOD'
-            },
-        ]
+        settings: settingsPEAK
     });
 }
 
 function postCreate() {
     for (item in alphabets.members) {
-        if (item.title.text == __volumeUp) {
-            keyUpdate(item, FlxG.save.data.volumeUpKeys);
-            continue;
-        } 
-        if (item.title.text == __volumeDown) {
-            keyUpdate(item, FlxG.save.data.volumeDownKeys);
-            continue;
+        for (data in __data) {
+            if (item.title.text != data.name) continue;
+            keyUpdate(item, Reflect.getProperty(FlxG.save.data, data.save));
         }
     }
 }
 
 function keyUpdate(item, options) {
-    var updateText = () -> {};
-    for (idx=>data in options) {
-        switch (data) {
-            case FlxKey.NUMPADPLUS:
-                var bind = (idx == 0) ? item.bind1 : item.bind2;
-                options[idx] = FlxKey.PLUS;
-                updateText = () -> options[idx] = FlxKey.NUMPADPLUS;
-            case FlxKey.NUMPADMINUS:
-                options[idx] = FlxKey.MINUS;
-                updateText = () -> options[idx] = FlxKey.NUMPADMINUS;
-        }
-    }
     item.option1 = options[0];
     item.option2 = options[1];
     item.updateText();
-    updateText();
 }
 
 var init = false;
@@ -58,30 +48,38 @@ var flicker:FlxFlicker;
 function preUpdate() {
     var text = alphabets.members[curSelected].title.text;
 
+
+    // Cant fix the bug where Flixel uses the input and changes the volume. Sorry chat :(
     if (!canSelect && FlxG.keys.justPressed.ANY) {
         flicker?.stop();
         canSelect = skipThisFrame = true;
         var index = alphabets.members[curSelected].p2Selected ? 1 : 0;
-        switch (text) {
-            case __volumeUp:
-                FlxG.save.data.volumeUpKeys[index] = FlxG.keys.firstJustPressed();
-                keyUpdate(alphabets.members[curSelected], FlxG.save.data.volumeUpKeys);
-            case __volumeDown:
-                FlxG.save.data.volumeDownKeys[index] = FlxG.keys.firstJustPressed();
-                keyUpdate(alphabets.members[curSelected], FlxG.save.data.volumeDownKeys);
+        for (data in __data) {
+            if (data.name != text) continue;
+            
+            var key = FlxG.keys.firstJustPressed();
+            var save = Reflect.getProperty(FlxG.save.data, data.save);
+            save[index] = key;
+
+            Reflect.setProperty(FlxG.save.data, data.save, save);
+            Reflect.setProperty(FlxG.sound, data.save, save);
+            FlxG.save.flush();
+
+            keyUpdate(alphabets.members[curSelected], Reflect.getProperty(FlxG.save.data, data.save));
+            break;
         }
     }
     
     if (controls.ACCEPT && !skipThisFrame && canSelect && init) {
-        if (text == __volumeUp || text == __volumeDown) skipThisFrame = true;
-        else return;
+        for (data in __data) {
+            if (data.name != text) continue;
+            skipThisFrame = true;
+            break;
+        }
+        if (!skipThisFrame) return;
         CoolUtil.playMenuSFX(1);
         flicker = FlxFlicker.flicker(alphabets.members[curSelected], 0, Options.flashingMenu ? 0.06 : 0.15, true, false, function(t) {});
         canSelect = false;
-        switch (text) {
-            case __volumeUp:
-                // trace('Volume Up');
-        }
     }
 
 
